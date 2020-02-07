cdf3ab7b6df52c336b1a961f18e7595955826a66
Vladimir Karetnikov <karetnikov.v.e@gmail.com>
2020-02-07 08:59:20 +0200

    task done

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Main from "../main/main.jsx";
+import PropTypes from "prop-types";
 
-const App = (props) => {
-  return (
-    // eslint-disable-next-line react/prop-types
-    <Main quantity={props.quantity}/>
-  );
+const App = ({quantity, placeNames}) => {
+  return <Main quantity={quantity} placeNames={placeNames} />;
+};
+
+App.propTypes = {
+  quantity: PropTypes.number.isRequired,
+  placeNames: PropTypes.array.isRequired,
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 const RENT_OFFERS_QUANTITY = 420;
+const PLACE_NAMES = [
+  `Beautiful &amp; luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`,
+  `Wood place`
+];
 
 ReactDOM.render(
-    <App quantity={RENT_OFFERS_QUANTITY} />,
+    <App quantity={RENT_OFFERS_QUANTITY} placeNames={PLACE_NAMES} />,
     document.querySelector(`#root`)
 );
